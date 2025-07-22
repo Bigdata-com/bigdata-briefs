85ce5a90dcc75da68a3a9745745dfe9be7b19017
jaldana <jaldana@ravenpack.com>
2025-07-22 10:15:26 +0200

    Make log level configurable and disable logs during testing

--- bigdata_briefs/__init__.py
+++ bigdata_briefs/__init__.py
@@ -1,10 +1,15 @@
 import logging
+import os
 from importlib.metadata import version
 
 import structlog
 
+LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO").upper()
+
 structlog.configure(
-    wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
+    wrapper_class=structlog.make_filtering_bound_logger(
+        logging._nameToLevel.get(LOG_LEVEL, logging.INFO)
+    ),
 )
 
 
