421f416a5e635ebd46fedcd2489693c22c8d5afc
jaldana <jaldana@ravenpack.com>
2025-08-12 13:36:11 +0200

    Remove dependabot asignee and ensure correct log level on web server

--- bigdata_briefs/__main__.py
+++ bigdata_briefs/__main__.py
@@ -1,6 +1,7 @@
+from bigdata_briefs import LOG_LEVEL
 from bigdata_briefs.api.app import app
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level=LOG_LEVEL.lower())
