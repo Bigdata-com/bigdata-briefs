eb6f009f1e71761e9c098eb2333551558e95428c
jaldana <jaldana@ravenpack.com>
2025-11-03 15:39:13 +0100

    Add support for demo mode in the backend

--- bigdata_briefs/api/app.py
+++ bigdata_briefs/api/app.py
@@ -129,6 +129,7 @@ async def sample_frontend(_: str = Security(query_scheme)) -> HTMLResponse:
             sources=example_values["sources"],
             example_watchlists=list(dict(ExampleWatchlists).values()),
             example_request_id=str(EXAMPLE_UUID),
+            demo_mode=settings.DEMO_MODE,
         ),
         media_type="text/html",
     )
