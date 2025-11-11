d7aacceffae617f797d4eb09df19b7880f638dc1
Paco Gomez <fgomez@ravenpack.com>
2025-11-11 17:37:02 +0100

    front-end

--- bigdata_briefs/__main__.py
+++ bigdata_briefs/__main__.py
@@ -4,4 +4,4 @@ from bigdata_briefs.api.app import app
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level=LOG_LEVEL.lower())
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level=LOG_LEVEL.lower())
